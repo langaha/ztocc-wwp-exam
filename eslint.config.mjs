19bb42c7e17359e658c07465f9323620a617d248
weiweiping <weiweiping@ztocc.com>
2026-05-07 20:46:12 +0800

    fix: eslint flat config for next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,17 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import { globalIgnores } from "eslint/config";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-function toConfigArray(v) {
-  if (Array.isArray(v)) return v;
-  if (!v) return [];
-  return [v];
-}
+const baseDirectory = dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory });
 
-const eslintConfig = defineConfig([
-  ...toConfigArray(nextVitals),
-  ...toConfigArray(nextTs),
-  // Override default ignores of eslint-config-next.
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
-]);
-
-export default eslintConfig;
+];
